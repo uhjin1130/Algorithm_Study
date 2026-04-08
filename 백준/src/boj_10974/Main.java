e90e6143f15d919954269860bfa899d2a3cdd668
uhjin1130 <rladjwls1130@naver.com>
2026-04-08 10:33:32 +0900

    10974

---
+++ 백준/src/boj_10974/Main.java
@@ -0,0 +1,40 @@
+package boj_10974;
+
+import java.io.*;
+
+public class Main {
+    public static int N;
+    public static int[] arr;
+    public static boolean[] visited;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args)  throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        visited = new boolean[N + 1];
+
+        dfs(0);
+
+        System.out.println(sb.toString());
+
+    }
+
+    public static void dfs(int depth){
+        if(depth == N) {
+            for(int i : arr){
+                sb.append(i + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 1; i <= N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                arr[depth] = i;
+                dfs(depth+1);
+                visited[i] = false;
+            }
+        }
+    }
+}
