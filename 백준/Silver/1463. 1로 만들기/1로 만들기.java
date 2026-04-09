90f1f5a16c8138438b948deb0c2eeac271eec62a
uhjin1130 <rladjwls1130@naver.com>
2026-04-09 15:34:48 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -6,29 +6,20 @@ import java.util.StringTokenizer;
 public class Main {
 	private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 	private int x;
-	private int dp[];
 
 	public Main() throws IOException {
 		StringTokenizer st = new StringTokenizer(bf.readLine());
 		x = Integer.parseInt(st.nextToken());
-		dp = new int[x + 1];
-		dp[0] = dp[1] = 0;
-		
-		caculate();
-		System.out.print(dp[x]);
+
+		System.out.print(caculate(x, 0));
 
 	}
 
-	public void caculate() {
-		for (int i = 2; i <= x; i++) {
-			dp[i] = dp[i - 1] + 1;
-			if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
-				dp[i] = dp[i/2] + 1;
-			}
-			if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
-				dp[i] = dp[i/3] + 1;
-			}
+	public int caculate(int x, int count) {
+		if(x<2) {
+			return count;
 		}
+		return Math.min(caculate(x/2,count+1+(x%2)), caculate(x/3,count+1+(x%3)));
 	}
 
 	public static void main(String[] args) throws IOException {
