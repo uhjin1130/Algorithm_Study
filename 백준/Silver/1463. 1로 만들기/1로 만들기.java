205bcc114995234921cd98284bcca73005077a55
uhjin1130 <rladjwls1130@naver.com>
2026-04-09 15:34:46 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 18048 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,32 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static int MAX = 1000000;
+	private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	private int x;
+	private int dp[];
 
-    public static int[] dp;
-    public static int N;
+	public Main() throws IOException {
+		StringTokenizer st = new StringTokenizer(bf.readLine());
+		x = Integer.parseInt(st.nextToken());
+		dp = new int[x + 1];
+		dp[0] = dp[1] = 0;
+		
+		caculate();
+		System.out.print(dp[x]);
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
+	}
 
-        dp = new int[N + 1];
-        dp[0] = dp[1] = 0;
+	public void caculate() {
+		for (int i = 2; i <= x; i++) {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 2 == 0 && dp[i] > dp[i / 2] + 1) {
+				dp[i] = dp[i/2] + 1;
+			}
+			if (i % 3 == 0 && dp[i] > dp[i / 3] + 1) {
+				dp[i] = dp[i/3] + 1;
+			}
+		}
+	}
 
-        calculate();
-        System.out.println(dp[N]);
-
-    }
-
-    public static void calculate() {
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;
-            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1)
-                dp[i] = dp[i / 2] + 1;
-            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1)
-                dp[i] = dp[i / 3] + 1;
-        }
-    }
-    
+	public static void main(String[] args) throws IOException {
+		new Main();
+	}
 }
