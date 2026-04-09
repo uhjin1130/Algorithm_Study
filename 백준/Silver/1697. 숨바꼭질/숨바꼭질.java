4f68d88d6ef396db95b457aaa07ddf7ab81d59a9
uhjin1130 <rladjwls1130@naver.com>
2026-04-09 15:33:11 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 18656 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,36 +1,49 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Main{
-    static int n,k;
-    static int[] check;
-    static int result = 0;
+public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        check = new int[100001];
-        Arrays.fill(check, -1);
-        bfs();
-        System.out.print(result);
-    }
-    public static void bfs(){
-        Queue<Integer> queue = new LinkedList<>();
-        check[n] = 0;
-        queue.add(n);
-        
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
-            if(cur==k){
-                result = check[cur];
-                return;
-            }
-            int[] nxt = {cur-1,cur+1,cur*2};
-            for(int i : nxt){
-                if(i>=0 && i<=100000 && check[i] == -1){
-                    check[i] = check[cur] + 1;
-                    queue.add(i);
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        if (n == k) {
+            System.out.println(0);
+            return;
+        }
+
+        int cnt = 0;
+        Queue<Integer> find = new LinkedList<>();
+        boolean[] check = new boolean[100001];
+
+        find.add(n);
+        check[n] = true;
+
+        while (!find.isEmpty()) {
+            int size = find.size();
+            cnt++;
+            for (int i = 0; i < size; i++) {
+                int x = find.remove();
+
+                if (x - 1 == k || x + 1 == k || x * 2 == k) {
+                    System.out.println(cnt);
+                    return;
+                }
+
+                if (x - 1 >= 0 && !check[x - 1]) {
+                    check[x - 1] = true;
+                    find.add(x - 1);
+                }
+                if (x + 1 <= 100000 && !check[x + 1]) {
+                    check[x + 1] = true;
+                    find.add(x + 1);
+                }
+                if (x * 2 <= 100000 && !check[x * 2]) {
+                    check[x * 2] = true;
+                    find.add(x * 2);
                 }
             }
         }
