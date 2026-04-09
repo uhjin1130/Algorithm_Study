ccc74b36fd4a8231b9bed641adb7481052181951
uhjin1130 <rladjwls1130@naver.com>
2026-04-09 15:28:41 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18044 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,27 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+    public static int MAX = 1000000;
+
     public static int[] dp;
     public static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        dp = new int[N+1];
+        dp = new int[N + 1];
         dp[0] = dp[1] = 0;
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1; // 1을 뺀값
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1); // 1을 뺀값과 2로 나눈값준 최솟값
-            }
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1); // (1을 뺀 값과 2로 나눈 값중 최솟값) 과 3으로 나눈값중 최솟값
-            }
-        }
-
+        calculate();
         System.out.println(dp[N]);
 
-    } // main
-} // class
+    }
+
+    public static void calculate() {
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0 && dp[i] > dp[i / 2] + 1)
+                dp[i] = dp[i / 2] + 1;
+            if (i % 3 == 0 && dp[i] > dp[i / 3] + 1)
+                dp[i] = dp[i / 3] + 1;
+        }
+    }
+    
+}
