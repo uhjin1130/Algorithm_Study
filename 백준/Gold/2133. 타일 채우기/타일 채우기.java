089a74c7cf6f96a7b5e9d990662db767b8bdb189
uhjin1130 <rladjwls1130@naver.com>
2026-04-09 15:27:51 +0900

    [Gold IV] Title: 타일 채우기, Time: 100 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if(N%2!=0){
+            System.out.println(0);
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+
+        dp[0] = 1;
+        dp[1] = 0;
+        dp[2] = 3;
+
+        for (int i = 4; i <= N; i+=2) {
+            dp[i] = dp[i-2] * dp[2];
+            for(int j = i-4; j > 0; j-=2) {
+                dp[i] += dp[j] * 2;
+            }
+            dp[i] += 2;
+        }
+
+        System.out.println(dp[N]);
+
+    }
+}
