b9362acb907018124a4cfdd75dcd29992d7e9163
uhjin1130 <rladjwls1130@naver.com>
2026-04-09 15:32:31 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,12 +2,13 @@ import java.util.*;
 import java.io.*;
 import java.awt.*;
 
+
 public class Main{
-    public static int n, m;
-    public static int[][] map;
-    public static boolean[][] visit;
-    public static int[] dx = { 0, 0, -1, 1};
-    public static int[] dy = { -1, 1, 0, 0};
+    static int n, m;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,33 +18,33 @@ public class Main{
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
-        visit = new boolean[n][m];
+        visited = new boolean[n][m];
 
-        for(int i = 0; i < n; i++){
-            String line = br.readLine();
-            for(int j = 0; j < m; j++){
-                map[i][j] = line.charAt(j) - '0';
+        for(int i = 0 ; i < n; i++){
+            String s = br.readLine();
+            for(int j = 0 ; j < m; j++){
+                map[i][j] = s.charAt(j) - '0';
             }
         }
-        
+
         bfs(0, 0);
         System.out.println(map[n-1][m-1]);
     }
 
     public static void bfs(int x, int y){
-        Queue<Point> queue = new LinkedList<>();
-        queue.add(new Point(x, y));
-        visit[x][y] = true;
+        Queue<Point> q = new LinkedList<>();
+        visited[x][y] = true;
+        q.add(new Point(x, y));
 
-        while(!queue.isEmpty()){
-            Point cur = queue.poll();
-            for(int i = 0; i < 4; i++){
+        while(!q.isEmpty()){
+            Point cur = q.poll();
+            for(int i = 0 ; i < 4; i++){
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
 
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && !visit[nx][ny] && map[nx][ny] > 0){
-                    queue.add(new Point(nx, ny));
-                    visit[nx][ny] = true;
+                if(0<=nx && nx<n && 0<=ny && ny<m && !visited[nx][ny] && map[nx][ny]>0){
+                    q.add(new Point(nx, ny));
+                    visited[nx][ny] = true;
                     map[nx][ny] = map[cur.x][cur.y] + 1;
                 }
             }
