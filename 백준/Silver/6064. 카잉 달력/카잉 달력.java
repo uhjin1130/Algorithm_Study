c7fbb6ce64453ebca01f090a6d586edf455d50e7
uhjin1130 <rladjwls1130@naver.com>
2026-04-09 15:27:31 +0900

    [Silver I] Title: 카잉 달력, Time: 240 ms, Memory: 18920 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int lcm = M * N / gcd(M, N);
+            x--;
+            y--;
+            int year = x;
+
+            while (year <= lcm) {
+                if (year % N == y) {
+                    System.out.println(year + 1);
+                    break;
+                }
+                year += M;
+            }
+
+            if(year > lcm)
+                System.out.println(-1);
+        }
+
+    }
+
+    public static int gcd(int a, int b) {
+        if (a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+
+}
