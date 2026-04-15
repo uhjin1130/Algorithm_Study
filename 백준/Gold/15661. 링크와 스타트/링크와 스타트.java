ac33423dd738571fe608f1578786991a30dab022
uhjin1130 <rladjwls1130@naver.com>
2026-04-15 10:26:12 +0900

    [Gold V] Title: 링크와 스타트, Time: 1304 ms, Memory: 23220 KB -BaekjoonHub

---
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static int N, result;
+    public static int[][] S;
+    public static boolean[] selected;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        S = new int[N][N];
+        selected = new boolean[N];
+        result = Integer.MAX_VALUE;
+
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                S[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        back(0);
+        System.out.println(result);
+
+        br.close();
+    }
+
+    public static void back(int idx) {
+        if (idx == N) {
+            calc();
+            return;
+        }
+
+        selected[idx] = true;
+        back(idx + 1);
+
+        selected[idx] = false;
+        back(idx + 1);
+    }
+
+    public static void calc() {
+        int start = 0, link = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (selected[i] && selected[j])
+                    start += S[i][j] + S[j][i];
+                if (!selected[i] && !selected[j])
+                    link += S[i][j] + S[j][i];
+            }
+        }
+
+        if(start < 1 || link < 1)
+            return;
+
+        result = Math.min(result, Math.abs(start - link));
+    }
+}
