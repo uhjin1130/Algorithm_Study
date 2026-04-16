0f2502859a85f453278f04666724a8fbd4b6a2a0
uhjin1130 <rladjwls1130@naver.com>
2026-04-16 13:25:29 +0900

    [Gold V] Title: 회의실 배정, Time: 472 ms, Memory: 44436 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,39 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int[][] time = new int[n][2];
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            time[i][0] = Integer.parseInt(st.nextToken());
-            time[i][1] = Integer.parseInt(st.nextToken());
-        }
+		BufferedReader bufRead = new BufferedReader( new InputStreamReader(System.in) );
+		StringTokenizer strToken = null;
 
-        Arrays.sort(time, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                if (a[1] == b[1]) {
-                    return a[0] - b[0];
-                }
-                return a[1] - b[1];
-            }
-        });
+		int N = Integer.parseInt( bufRead.readLine() );
+		int [][] time = new int[N][];
 
-        int count = 0;
-        int prev_end_time = 0;
+		for (int i = 0; i < N; i++) {
+			strToken = new StringTokenizer( bufRead.readLine() );
+			time[i] = new int[] {Integer.parseInt( strToken.nextToken() )
+								, Integer.parseInt( strToken.nextToken() )};
+		} // for - time[i][0] : 시작 시간, time[i][1] : 종료 시간
 
-        for (int i = 0; i < time.length; i++) {
-            if (prev_end_time <= time[i][0]) {
-                count++;
-                prev_end_time = time[i][1];
-            }
-        }
+		bufRead.close();
 
-        System.out.println(count);
-    }
+		Arrays.sort(time, (o1, o2)
+							-> o1[1] == o2[1]? Integer.compare(o1[0], o2[0]) : Integer.compare(o1[1], o2[1])
+		); // Arrays.sort - 끝나는 시간이 같으면 시작 시간으로 정렬, 다르면 종료 시간으로 정렬.
+
+		int [] end = time[0]; // time[0] : 종료 시간이 가장 빠른 회의를 넣고 시작.
+		int cnt = 1;
+
+		for (int i = 1; i < time.length; i++) {
+			if( end[1] <= time[i][0] ) { // end의 종료 시간보다 time[i]의 시작 시간이 늦다. -> 겹치지 않음.
+				end = time[i];
+				cnt++;
+			} // if
+		} // for
+
+		// 결과 값 출력.
+		StringBuilder strBuild = new StringBuilder();
+		strBuild.append( cnt );
+		System.out.println( strBuild.toString() );
+
+	} // main
+
 }
