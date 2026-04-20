96b5d904c07316a8806bbb5c7f4ec2503d090f70
uhjin1130 <rladjwls1130@naver.com>
2026-04-20 10:17:08 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 668 ms, Memory: 52732 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,97 +1,85 @@
-import java.awt.Point;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    static int n, m; // 세로, 가로
+    public static int n, m, startX, startY;
+    public static int[][] map, result;
+    public static Queue<int[]> queue = new LinkedList<>();
+    public static boolean[][] visited;
+    public static int[] dx = { 0, 0, -1, 1 };
+    public static int[] dy = { -1, 1, 0, 0 };
+    public static StringBuilder sb = new StringBuilder();
 
-    static class Graph {
-        private int Map[][];
-        private boolean visit[][];
-        private Queue<Point> queue;
-        private int dirX[] = {0, 0, -1, 1}; // 위, 아래, 왼쪽, 오른쪽
-        private int dirY[] = {-1, 1, 0, 0}; // 위, 아래, 왼쪽, 오른쪽
-        private int startX, startY;
-        private int result[][];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        public Graph() throws IOException {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            m = Integer.parseInt(st.nextToken());
-
-            visit = new boolean[n][m];
-            Map = new int[n][m];
-            queue = new LinkedList<>();
-
-            result = new int[n][m];
-
-            setMap(br);
-            setResult();
-            getResult();
-        }
-
-        public void setMap(BufferedReader br) throws IOException {
-            for (int i = 0; i < n; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < m; j++) {
-                    Map[i][j] = Integer.parseInt(st.nextToken());
-                    if (Map[i][j] == 2) {
-                        startX = j;
-                        startY = i;
-                    }
+        visited = new boolean[n][m];
+        map = new int[n][m];
+        result = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                map[i][j] = num;
+                if (num == 2) {
+                    startX = j;
+                    startY = i;
+                }
+                if (num == 0) {
+                    visited[i][j] = true; // 0은 visited true
+                } else if (num == 1) {
+                    result[i][j] = -1; // ★ 1은 -1 초기화
                 }
             }
         }
 
-        public void setResult() {
-            int x = startX;
-            int y = startY;
+        result[startY][startX] = 0;
 
-            queue.add(new Point(x, y));
-            visit[y][x] = true;
+        navigation(startX, startY);
 
-            Point current;
-
-            while (!queue.isEmpty()) {
-                current = queue.poll();
-
-                for (int i = 0; i < 4; i++) {
-                    int nextX = current.x + dirX[i];
-                    int nextY = current.y + dirY[i];
-
-                    if (nextX >= 0 && nextY >= 0 && nextX < m && nextY < n && !visit[nextY][nextX] && Map[nextY][nextX] == 1) {
-                        queue.add(new Point(nextX, nextY));
-                        visit[nextY][nextX] = true;
-                        result[nextY][nextX] = result[current.y][current.x] + 1;
-                    }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (visited[i][j] && map[i][j] == 1) {
+                    sb.append(result[i][j]).append(" ");
+                } else if (!visited[i][j] && map[i][j] == 1) {
+                    sb.append(-1 + " ");
+                } else {
+                    sb.append(0).append(" ");
                 }
             }
+            sb.append("\n");
         }
 
-        public void getResult() {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (visit[i][j] && Map[i][j] == 1) {
-                        System.out.print(result[i][j] + " ");
-                    } else if (!visit[i][j] && Map[i][j] == 1) {
-                        System.out.print(-1 + " ");
-                    } else {
-                        System.out.print(0 + " ");
-                    }
+        System.out.println(sb.toString());
+
+    }
+
+    public static void navigation(int sx, int sy) {
+        queue.add(new int[] { sy, sx });
+        visited[sy][sx] = true;
+        result[sy][sx] = 0;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int cy = current[0];
+            int cx = current[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < m && ny < n && map[ny][nx] == 1 && !visited[ny][nx]) {
+                    visited[ny][nx] = true;
+                    result[ny][nx] = result[cy][cx] + 1;
+                    queue.add(new int[] { ny, nx });
                 }
-                System.out.println();
             }
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        Graph graph = new Graph();
-    }
 }
