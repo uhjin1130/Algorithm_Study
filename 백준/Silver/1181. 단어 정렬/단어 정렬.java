4358c09acd7fe4363ef79b9b7b360b7b2cea6567
uhjin1130 <rladjwls1130@naver.com>
2026-04-22 15:13:50 +0900

    [Silver V] Title: 단어 정렬, Time: 304 ms, Memory: 26248 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
+        String[] arr = new String[N];
+
+        for(int i = 0; i < N; i++){
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                if(s1.length() == s2.length()) {
+                    return s1.compareTo(s2);
+                } else {
+                    return s1.length() - s2.length();
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(arr[0]).append("\n");
+        
+        for(int i = 1; i < N; i++){
+            if(!arr[i].equals(arr[i-1])){
+                sb.append(arr[i]).append("\n");
+            }
+        }
+
+        System.out.println(sb.toString());
+
+    } // main
+
+} // class
