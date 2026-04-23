a1e493525ef69409fd0d7d12340a2ef65517e242
uhjin1130 <rladjwls1130@naver.com>
2026-04-23 09:21:10 +0900

    [Silver I] Title: 스타트와 링크, Time: 484 ms, Memory: 22356 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -3,16 +3,16 @@ import java.util.*;
 
 public class Main {
 
-    public static int N, result;
+    public static int N;
+    public static int result = Integer.MAX_VALUE;
     public static int[][] S;
     public static boolean[] selected;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         S = new int[N][N];
         selected = new boolean[N];
-        result = Integer.MAX_VALUE;
 
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
@@ -24,35 +24,41 @@ public class Main {
 
         back(0, 0);
         System.out.println(result);
-    }
+
+    } // main
 
     public static void back(int idx, int cnt) {
-        if(cnt == N / 2){
+        if (cnt == N / 2) {
             calc();
             return;
         }
-        if(idx == N){
+
+        if (idx == N)
             return;
-        }
 
         selected[idx] = true;
         back(idx + 1, cnt + 1);
 
         selected[idx] = false;
         back(idx + 1, cnt);
-        
+
     }
 
-    public static void calc(){
-        int start = 0, link = 0;
-        for(int i = 0; i < N; i++){
-            for(int j = i + 1; j < N; j++){
-                if(selected[i] && selected[j])
+    public static void calc() {
+        int start = 0;
+        int link = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (selected[i] && selected[j])
                     start += S[i][j] + S[j][i];
-                if(!selected[i] && !selected[j])
+                if (!selected[i] && !selected[j])
                     link += S[i][j] + S[j][i];
             }
         }
+
         result = Math.min(result, Math.abs(start - link));
+
     }
-}
+
+} // class
