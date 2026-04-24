5d7c9532b93a6213c287955aec51d820a8470ff7
uhjin1130 <rladjwls1130@naver.com>
2026-04-24 14:28:15 +0900

    [Silver II] Title: 에디터, Time: 668 ms, Memory: 160452 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -2,45 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Deque<Character> left = new ArrayDeque<>(), right = new ArrayDeque<>();
+        LinkedList<Character> left = new LinkedList<>(), right = new LinkedList<>();
 
         String s = br.readLine();
-        for(char c : s.toCharArray()) 
-            left.addLast(c);
+        for (char c : s.toCharArray()) {
+            left.add(c);
+        }
 
         int n = Integer.parseInt(br.readLine());
-        for(int i = 0; i < n; i++) {
-            String[] tokens = br.readLine().trim().split(" ");
-            String cmd = tokens[0];
-            switch(cmd) {
-                case "P":
-                    left.addLast(tokens[1].charAt(0));
-                    break;
+        for (int i = 0; i < n; i++) {
+            String[] op = br.readLine().trim().split(" ");
+            switch (op[0]) {
                 case "L":
-                    if(!left.isEmpty()) 
+                    if(!left.isEmpty())
                         right.addFirst(left.removeLast());
                     break;
                 case "D":
-                    if(!right.isEmpty()) 
-                        left.addLast(right.removeFirst());
+                    if(!right.isEmpty())
+                        left.add(right.removeFirst());
                     break;
                 case "B":
-                    if(!left.isEmpty()) 
+                    if(!left.isEmpty())
                         left.removeLast();
+                    break;
+                case "P":
+                    left.addLast(op[1].charAt(0));
+                    break;
+
+                default:
                     break;
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        
-        for(char c : left) 
-            sb.append(c);
-        
-        while(!right.isEmpty()) 
-            sb.append(right.removeFirst());
-        
-        System.out.println(sb);
+        for(char cl : left)
+            sb.append(cl);
+        for(char cr : right)
+            sb.append(cr);
+
+        System.out.println(sb.toString());
+
     }
+
 }
