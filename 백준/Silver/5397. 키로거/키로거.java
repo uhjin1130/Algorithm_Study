51f215b60c979063048c376b03605975f6f4b6da
uhjin1130 <rladjwls1130@naver.com>
2026-04-24 09:07:24 +0900

    [Silver II] Title: 키로거, Time: 2468 ms, Memory: 135148 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,12 +1,56 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static LinkedList<Character> password;
+    public static char[] keyLog;
+    public static int T;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            password = new LinkedList<>();
+            keyLog = br.readLine().toCharArray();
+            solve();
+        }
+
+    }
+
+    public static void solve() {
+        int head = 0;
+        int cnt = 0;
+
+        while (cnt < keyLog.length) {
+            char c = keyLog[cnt];
+            switch (c) {
+                case '<':
+                    if (head > 0)
+                        head--;
+                    break;
+                case '>':
+                    if (head < password.size())
+                        head++;
+                    break;
+                case '-':
+                    if (head > 0) {
+                        password.remove(head - 1);
+                        head--;
+                    }
+                    break;
+                default:
+                    password.add(head, c);
+                    head++;
+                    break;
+            }
+            cnt++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (char ch : password)
+            sb.append(ch);
+        System.out.println(sb);
+    }
+
+}
