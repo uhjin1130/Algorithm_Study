1dd2a7fc1d17f1d3921c0c0656fbe6c468eb2155
uhjin1130 <rladjwls1130@naver.com>
2026-04-27 13:16:06 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,53 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-	static boolean[] visitied;
-	static ArrayList<Integer>[] arr;
+    public static int N, M, cnt;
+    public static int[][] computers;
+    public static boolean[] visitied;
 
-	public Main() throws IOException {
-		int N = Integer.parseInt(bf.readLine());
-		int connect_number = Integer.parseInt(bf.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
 
-		visitied = new boolean[N + 1];
-		arr = new ArrayList[N + 1];
+        computers = new int[N + 1][N + 1];
+        visitied = new boolean[N + 1];
 
-		setBridge(N, connect_number);
-		System.out.print(dfs(1) - 1);
-	}
+        for (int i = 1; i <= M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-	public void setBridge(int N, int connect) throws IOException {
-		for(int i=1;i<=N;i++) {
-			arr[i] = new ArrayList<>();
-		}
-		
-		for(int i=1;i<=connect;i++) {
-			StringTokenizer st = new StringTokenizer(bf.readLine());
-			int u = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
-			arr[u].add(v);
-			arr[v].add(u);
-		}
-	}
-	
-	public int dfs(int node) {
-		if(visitied[node])
-			return 0;
-		visitied[node] = true;
-		int count = 1;
-		for(int i : arr[node]) {
-			if(visitied[i] == false) {
-				count += dfs(i);
-			}
-		}
-		return count;
-	}
+            computers[a][b] = computers[b][a] = 1;
+        }
 
-	public static void main(String[] args) throws IOException {
-		new Main();
-	}
+        cnt = 0;
+        dfs(1);
+
+        System.out.println(cnt);
+    }
+
+    public static void dfs(int node) {
+        visitied[node] = true;
+
+        for (int i = 1; i <= N; i++) {
+            if (!visitied[i] && computers[node][i] == 1) {
+                cnt++;
+                dfs(i);
+            }
+        }
+    }
+
 }
