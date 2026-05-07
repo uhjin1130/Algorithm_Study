28f3fee60aa889158b6c9bbb647a904651850eaa
uhjin1130 <rladjwls1130@naver.com>
2026-05-07 10:28:46 +0900

    백준 1로 만들기 코드 개선

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,28 +1,23 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-	private int x;
-
-	public Main() throws IOException {
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		x = Integer.parseInt(st.nextToken());
-
-		System.out.print(caculate(x, 0));
-
-	}
-
-	public int caculate(int x, int count) {
-		if(x<2) {
-			return count;
-		}
-		return Math.min(caculate(x/2,count+1+(x%2)), caculate(x/3,count+1+(x%3)));
-	}
 
 	public static void main(String[] args) throws IOException {
-		new Main();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		int result = caculate(N, 0);
+
+		System.out.println(result);
+
+	} // main
+
+	public static int caculate(int x, int count) {
+		if (x < 2)
+			return count;
+
+		return Math.min(caculate(x / 2, count + 1 + (x % 2)), caculate(x / 3, count + 1 + (x % 3)));
 	}
-}
+
+} // class
--- 백준/src/Main.java
+++ 백준/src/Main.java
@@ -2,73 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static StringBuilder sb = new StringBuilder();
-    public static int N, E;
-    public static int[][] graph;
-    public static int[] nodes;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
 
-        nodes = new int[N + 1];
-        graph = new int[E + 1][3];
+        int result = caculate(N, 0);
 
-        for (int i = 0; i <= N; i++) {
-            nodes[i] = i;
-        }
-
-        for (int i = 1; i <= E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-
-            graph[i][0] = from;
-            graph[i][1] = to;
-            graph[i][2] = weight;
-        }
-
-        Arrays.sort(graph, 1, E + 1, (o1, o2) -> Integer.compare(o1[2], o2[2]));
-
-        kruskal();
+        System.out.println(result);
 
     } // main
 
-    public static void kruskal() {
-        int cost = 0;
-        int cnt = 0;
-        for (int i = 1; i <= E; i++) {
-            if (find(graph[i][0]) != find(graph[i][1])) {
-                union(graph[i][0], graph[i][1]);
-                cnt++;
-                cost += graph[i][2];
+    public static int caculate(int x, int count) {
+        if (x < 2)
+            return count;
 
-                if (cnt == N - 1)
-                    break;
-            }
-        }
-
-        System.out.println(cost);
-    }
-
-    public static void union(int a, int b) {
-        a = find(a);
-        b = find(b);
-
-        if (a < b)
-            nodes[b] = a;
-        else
-            nodes[a] = b;
-    }
-
-    public static int find(int x) {
-        if (nodes[x] == x)
-            return x;
-        else
-            return find(nodes[x]);
+        return Math.min(caculate(x / 2, count + 1 + (x % 2)), caculate(x / 3, count + 1 + (x % 3)));
     }
 
 } // class
