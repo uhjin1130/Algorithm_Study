254f7fe8c5ffcadb9e2d650091a29e87b8cedb84
uhjin1130 <rladjwls1130@naver.com>
2026-06-25 15:03:51 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<Integer> solution(int n) {
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i+=2) {
+            answer.add(i);
+        }
+        
+        return answer;
+    }
+}
